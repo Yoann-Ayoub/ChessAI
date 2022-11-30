6807a4f44ef02885ecc99a854239d6b395f7bcc1
yoann <yoannayoub@gmail.com>
2022-11-30 16:40:53 -0500

    nettoyage BoardGeneration

--- src/main/java/org/example/UCI.java
+++ src/main/java/org/example/UCI.java
@@ -36,10 +36,12 @@ public class UCI {
             {
                 inputQuit();
             }
+            /*
             else if ("print".equals(inputString))
             {
                 inputPrint();
             }
+             */
         }
     }
     public static void inputUCI() {
@@ -180,7 +182,10 @@ public class UCI {
     public static void inputQuit() {
         System.exit(0);
     }
+    /*
     public static void inputPrint() {
         BoardGeneration.drawArray(UserInterface.WP,UserInterface.WN,UserInterface.WB,UserInterface.WR,UserInterface.WQ,UserInterface.WK,UserInterface.BP,UserInterface.BN,UserInterface.BB,UserInterface.BR,UserInterface.BQ,UserInterface.BK);
     }
+     */
+
 }
